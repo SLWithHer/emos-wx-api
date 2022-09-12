9212e5bc5dd3f2c0db19982fd804acec9b295bd3
letian-ready <ready_for_love@163.com>
2022-09-13 00:44:28 +0800

    Initial commit

---
+++ src/main/java/com/example/emos/ex/EmosWxApiApplication.java
@@ -0,0 +1,15 @@
+package com.example.emos.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author SULE
+ */
+@SpringBootApplication
+public class EmosWxApiApplication {
+
+  public static void main(String[] args) {
+      SpringApplication.run (EmosWxApiApplication.class, args);
+  }
+}
